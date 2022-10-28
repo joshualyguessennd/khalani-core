8f0ae906547880989eb1ec8b4e87f4673ac15cc5
Defcon023 <>
2022-10-28 22:26:03 +0530

    Custody Facet - Diamond Storage

--- solidity/khalani-core/src/libraries/LibCustody.sol
+++ solidity/khalani-core/src/libraries/LibCustody.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.0;
 library LibCustody {
     bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("khalini.custody.storage");
 
-    struct CustodyState{
+    struct CustodyStorage{
+        address public gateway;
+        uint256 public number;
 
+        /// @dev stores all user balances
+        mapping(address => uint) public balances;
     }
 
-    function diamondStorage() internal pure returns (CustodyState storage ds) {
+
+
+    function custodyStorage() internal pure returns (CustodyStorage storage ds) {
         bytes32 position = DIAMOND_STORAGE_POSITION;
         assembly {
             ds.slot := position
