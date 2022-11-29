13d2fa77033a11c6fe1cb26c7d5aaae73ae38d9a
Defcon023 <>
2022-11-29 21:18:54 +0530

    Removing hyperlane

--- solidity/khalani-core/hyperlane-monorepo/rust/chains/abacus-ethereum/build.rs
+++
@@ -1,5 +0,0 @@
-use abigen::generate_bindings_for_dir;
-
-fn main() {
-    generate_bindings_for_dir("./abis", "./src/contracts");
-}
--- solidity/khalani-core/hyperlane-monorepo/typescript/infra/src/config/index.ts
+++
@@ -1,4 +0,0 @@
-export { AgentConfig, RustConfig } from './agent';
-export { CoreEnvironmentConfig, DeployEnvironment } from './environment';
-export { HelloWorldConfig } from './helloworld';
-export { InfrastructureConfig } from './infrastructure';
