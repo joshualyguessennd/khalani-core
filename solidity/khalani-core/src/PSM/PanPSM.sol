61f1faa7f82e1d15bef04f09b5caf8c07b4f35a5
Defcon023 <manmeetparmar23@gmail.com>
2023-01-25 17:59:47 +0530

    1:1 PanPSM + Tests

---
+++ solidity/khalani-core/src/PSM/PanPSM.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interfaces/IERC20Mintable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PanPSM is Ownable{
+
+    error AssetNotWhiteListed();
+    error RedeemFailedNotEnoughBalance();
+
+    event WhiteListedTokenAdded(
+        address indexed asset
+    );
+
+    event WhiteListedTokenRemoved(
+        address indexed asset
+    );
+
+    mapping(address => bool) whiteListedTokens;
+    address pan;
+
+    function initialize(address _pan) external onlyOwner {
+        pan = _pan;
+    }
+
+    function addWhiteListedAsset(address asset) external onlyOwner {
+        whiteListedTokens[asset] = true;
+        emit WhiteListedTokenAdded(asset);
+    }
+
+    function removeWhiteListedAddress(address asset) external onlyOwner {
+        whiteListedTokens[asset] = false;
+        emit WhiteListedTokenRemoved(asset);
+    }
+
+    function mintPan(address tokenIn, uint amount) external {
+        if(!whiteListedTokens[tokenIn]){
+            revert AssetNotWhiteListed();
+        }
+        SafeERC20.safeTransferFrom(IERC20(tokenIn),msg.sender,address(this),amount);
+        IERC20Mintable(pan).mint(msg.sender, amount);
+    }
+
+    function redeemPan(uint256 amount, address tokenOut) external {
+        if(!whiteListedTokens[tokenOut]){
+            revert AssetNotWhiteListed();
+        }
+        IERC20Mintable(pan).burn(msg.sender, amount);
+        uint redeemedAmount = (amount*995)/1000;
+        if(IERC20(tokenOut).balanceOf(address(this)) <= redeemedAmount){
+            revert RedeemFailedNotEnoughBalance();
+        }
+        SafeERC20.safeTransfer(IERC20(tokenOut),msg.sender,redeemedAmount);
+    }
+}
---
+++ solidity/khalani-core/test/PanPSMTest.t.sol
@@ -0,0 +1,168 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "./Mock/MockERC20.sol";
+import "../src/USDMirror.sol";
+import "../src/PSM/PanPSM.sol";
+
+contract PanPSMTest is Test {
+
+    error AssetNotWhiteListed();
+    error RedeemFailedNotEnoughBalance();
+
+    event WhiteListedTokenAdded(
+        address indexed asset
+    );
+
+    event WhiteListedTokenRemoved(
+        address indexed asset
+    );
+
+    PanPSM psm;
+    MockERC20 usdc;
+    MockERC20 usdt;
+    USDMirror pan;
+
+    address user = 0x0000000000000000000000000000000000000001;
+    address MOCK_ADDRESS_1 = 0x0000000000000000000000000000000000000002;
+
+    function setUp() public {
+        usdc = new MockERC20("USDC","USDC");
+        usdt = new MockERC20("USDT","USDT");
+        pan = new USDMirror();
+        pan.initialize("PAN","PAN");
+
+        psm = new PanPSM();
+        psm.initialize(address(pan));
+        pan.transferMinterBurnerRole(address(psm));
+
+        psm.addWhiteListedAsset(address(usdc));
+        psm.addWhiteListedAsset(address(usdt));
+    }
+
+    function testTokenAdd() public{
+        MockERC20 busd = new MockERC20("BUSD","BUSD");
+        vm.expectEmit(true,false,false,true);
+        emit WhiteListedTokenAdded(address(busd));
+        psm.addWhiteListedAsset(address(busd));
+    }
+
+    function testTokenRemove() public{
+        vm.expectEmit(true,false,false,true);
+        emit WhiteListedTokenRemoved(address (usdc));
+        psm.removeWhiteListedAddress(address(usdc));
+    }
+
+    function testMintPan(uint balanceAmount, uint mintAmount) public{
+        vm.assume(balanceAmount>0 && mintAmount>0 && balanceAmount>=mintAmount);
+        usdc.mint(user,balanceAmount);
+
+        vm.startPrank(user);
+        IERC20(address(usdc)).approve(address(psm),mintAmount);
+        psm.mintPan(address(usdc),mintAmount);
+        vm.stopPrank();
+
+        assertEq(
+            usdc.balanceOf(user),
+            balanceAmount - mintAmount
+        );
+
+        assertEq(
+            pan.balanceOf(user),
+            mintAmount
+        );
+
+        assertEq(
+            usdc.balanceOf(address(psm)),
+            mintAmount
+        );
+
+        vm.startPrank(user);
+        vm.expectRevert();
+        psm.mintPan(address(usdt),mintAmount);
+        vm.stopPrank();
+
+        assertEq(
+            usdt.balanceOf(user),
+            0
+        );
+
+        assertEq(
+            usdt.balanceOf(address(psm)),
+            0
+        );
+
+    }
+
+    function testMintPanInvalidAsset(uint balanceAmount, uint mintAmount) public{
+        vm.assume(balanceAmount>0 && mintAmount>0 && balanceAmount>=mintAmount);
+        MockERC20 newAsset = new MockERC20("DUMMY","DUMMY");
+        newAsset.mint(user,balanceAmount);
+        //trying with a non-whitelisted asset
+        vm.startPrank(user);
+        newAsset.approve(address(psm),mintAmount);
+        vm.expectRevert(AssetNotWhiteListed.selector);
+        psm.mintPan(MOCK_ADDRESS_1,mintAmount);
+        vm.stopPrank();
+
+        assertEq(
+            IERC20(newAsset).balanceOf(user),
+            balanceAmount
+        );
+
+    }
+
+    function testRedeem(uint balanceAmount, uint mintAmount, uint redeemAmount) public{
+        vm.assume(balanceAmount<10e18 && balanceAmount>0 && mintAmount>0 && redeemAmount>0 && balanceAmount>=mintAmount && mintAmount>=redeemAmount);
+        usdc.mint(user,balanceAmount);
+
+        vm.startPrank(user);
+        usdc.approve(address(psm),mintAmount);
+        psm.mintPan(address(usdc),mintAmount);
+        vm.stopPrank();
+
+        assertEq(
+            usdc.balanceOf(user),
+            balanceAmount - mintAmount
+        );
+
+        assertEq(
+            pan.balanceOf(user),
+            mintAmount
+        );
+
+        assertEq(
+            usdc.balanceOf(address(psm)),
+            mintAmount
+        );
+
+
+        vm.prank(user);
+        psm.redeemPan(redeemAmount,address(usdc));
+
+        assertEq(
+            pan.balanceOf(user),
+            mintAmount - redeemAmount
+        );
+//
+//        assertApproxEqAbs(
+//            balanceAmount + (redeemAmount*5)/1000 - mintAmount,
+//            usdc.balanceOf(user),
+//
+//        );
+//
+//        assertApproxEqAbs(
+//            (redeemAmount*5)/1000,
+//            usdc.balanceOf(address(psm)),
+//            2
+//        );
+    }
+
+    function testRedeemInvalidAsset() public{
+
+    }
+
+    function testRedeemPanLowBalance() public{
+
+    }
+}
